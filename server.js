fe1f756a23ffa988841d611aabc01cd4377b1e0d
William Fortin <willyfortin@gmail.com>
2013-03-07 00:23:09 -0500

    Added timeago plugin for dates

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ app.set( 'view engine', 'jade' );
 app.set( 'views', __dirname + '/views' );
 app.use( express.static( __dirname + '/public' ));
 app.use( app.router );
+app.configure('development', function(){
+  app.use(express.errorHandler());
+  app.locals.pretty = true;
+});
 
-app.get( '/', function ( req, res ) { res.render( 'index' ) });
+app.get( '/', function ( req, res ) { res.render( 'index' ); });
 
 app.listen( process.env.PORT || 3000 );
